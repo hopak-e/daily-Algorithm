230b30dc57ea5f80cab3e5721f606953171f2842
박상호 <a328028@gmail.com>
2022-06-06 13:58:12 +0900

    제주100 4번

--- 제주 1-20/4번/4.js
+++ 제주 1-20/4번/4.js
@@ -1,7 +1,7 @@
 // ? a를 typeof(a)로 넣었을 때 출력될 값과의 연결이 알맞지 않은것은 ?
 /* 
  1) 입 a = 1, 출 number
- ! 2) 입 a= 2.22, 출 boolean => number 
+ ! 2) 입 a = 2.22, 출 boolean => number 
  3) 입 a = 'p' , 출 string
  4) 입 a=[1,2,3], 출 object
 */
