6d808ce5ac1760209b40528eb0ff88f9976a6b77
박상호 <a328028@gmail.com>
2022-06-19 00:21:57 +0900

    제주100 17번

--- 제주 1-20/16번/16.js
+++ 제주 1-20/16번/16.js
@@ -2,4 +2,4 @@ function inverse(input) {
   return input.split("").reverse().join("");
 }
 
-console.log(inverse("거꾸로"));
+inverse("거꾸로");
---
+++ 제주 1-20/17번/17.js
@@ -0,0 +1,9 @@
+function ggoggoma(num) {
+  if (num >= 150) {
+    return "Yes";
+  } else {
+    return "NO";
+  }
+}
+
+console.log(ggoggoma(150));
