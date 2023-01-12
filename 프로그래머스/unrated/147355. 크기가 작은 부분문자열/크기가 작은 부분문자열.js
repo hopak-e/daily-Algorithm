5933c0dbdb0ecdbf7cef1d4c783f782dbc6a1f75
박상호 <97100045+hopak-e@users.noreply.github.com>
2023-01-12 15:21:20 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,12 @@
+function solution(t, p) {
+    var result;
+    var count = 0;
+    var slice = p.length;
+    for(let i=0; i<=t.length-slice; i++){
+        var answer = t.slice(i,i+slice);
+        if(answer <= p){
+            count++;
+        }
+    }
+    return count;
+}
