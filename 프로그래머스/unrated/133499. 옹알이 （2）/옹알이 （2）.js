2f6dbff7a07a2dbe2134c2211f52c333b6ad1fb4
박상호 <97100045+hopak-e@users.noreply.github.com>
2023-01-20 18:10:40 +0900

    [unrated] Title: 옹알이 (2), Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -0,0 +1,18 @@
+function solution(babbling) {
+    var answer = 0;
+    //aya ye
+    const arr = ["aya", "ye", "woo", "ma"]
+    for(let i=0; i<babbling.length; i++){
+        let bab=babbling[i]
+        for(let j=0; j<arr.length; j++){
+            if(bab.includes(arr[j].repeat(2))){
+                break;
+            }
+             bab = bab.split(arr[j]).join(" ")
+            }
+               
+               if(bab.split(" ").join("").length===0) answer++;
+            }
+    
+    return answer;
+}
