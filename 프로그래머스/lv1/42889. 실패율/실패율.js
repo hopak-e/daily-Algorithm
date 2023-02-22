1001df31d6ef3ce7c88189faf4b8725d03c8e3f0
박상호 <97100045+hopak-e@users.noreply.github.com>
2023-02-22 15:29:28 +0900

    [level 1] Title: 실패율, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,11 +1,5 @@
 function solution(N, stages) {
     var answer = [];
-    let people = stages.length;
-    for(let i=1; i<=N; i++){
-        let temp = stages.filter(el=>el===i).length;
-        answer.push([i, temp/people])
-        people -=temp
-    }
-    answer = answer.sort((a,b)=>b[1]-a[1])
-    return answer.map(a=>a[0]);
+    
+    return answer;
 }
