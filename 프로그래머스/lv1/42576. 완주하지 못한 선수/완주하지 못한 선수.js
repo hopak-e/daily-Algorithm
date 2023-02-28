c5343eb1663a239a2f2a1850077e404828972f9e
박상호 <97100045+hopak-e@users.noreply.github.com>
2023-03-01 02:38:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 29.72 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,9 +1,13 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-    for(var i=0;i<participant.length;i++){
-        if(participant[i] !== completion[i]){
-            return participant[i];
-        }
+   const map = new Map();
+
+    for(let i=0; i<participant.length; i++){
+        let a=participant[i],b=completion[i]
+        map.set(a,(map.get(a)||0)+1);
+        map.set(b,(map.get(b)||0)-1);
     }
+    for(let [v, k] of map){
+        if(k>0) return v
+    }
+    return "undefined"
 }
