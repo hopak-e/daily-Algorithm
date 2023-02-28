84f157378da52b5bb243aadb146c0380927266ff
박상호 <97100045+hopak-e@users.noreply.github.com>
2023-03-01 01:52:26 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,12 +1,13 @@
 function solution(citations) {
-    var answer = 0;
+  citations = citations.sort(sorting);
+  var i = 0;
 
-    for(let i=citations.length; i>=0; i--){
-        for(let j=0; j<citations.length; j++){
-            if(citations[j]>=i) answer++;
-        }
-        if(answer>=i) {answer=i; break};
-        answer = 0;
-    }
-    return answer;
+  while (i + 1 <= citations[i]) {
+    i++;
+  }
+  return i;
+
+  function sorting(a, b) {
+    return b - a;
+  }
 }
