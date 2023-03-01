c87ac91565b69d093d1a7ddc97fc917baed81fd6
박상호 <97100045+hopak-e@users.noreply.github.com>
2023-03-01 15:15:29 +0900

    [level 2] Title: 위장, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.js
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -1,12 +1,4 @@
 function solution(clothes) {
-    var answer = 1;
-    const map = new Map();
-    for(let i=0; i<clothes.length; i++){
-        let category = clothes[i][1]
-        map.set(category, (map.get(category)||0)+1)
-    }
-    for(let [k,v] of map){
-        answer*=v+1
-    }
-    return answer-1;
+    var answer = 0;
+    return answer;
 }
