ceaa07871f022898f1dd8c26300a475915bc9520
박상호 <97100045+hopak-e@users.noreply.github.com>
2023-03-03 01:02:56 +0900

    Time: 69 ms, Memory: 43.9 MB - LeetHub

---
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    const map = new Map();
+    for(let n of nums1){
+        if(map.has(n)){
+            map.set(n, map.get(n)+1)
+        } else {
+            map.set(n,1)
+        }
+    }
+    
+    const result = [];
+    
+    for(let n of nums2){
+        if(map.has(n) && map.get(n)>0){
+            result.push(n)
+            map.set(n,map.get(n)-1)
+        }
+    }
+    return result
+};
