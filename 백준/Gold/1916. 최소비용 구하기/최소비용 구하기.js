efa101ab01f2e6c51f1e7d5854ce8ad220b57375
박상호 <97100045+hopak-e@users.noreply.github.com>
2024-07-26 11:25:12 +0900

    [Gold V] Title: 최소비용 구하기, Time: 384 ms, Memory: 58172 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.js
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.js
@@ -6,106 +6,52 @@ let input = require("fs")
 
 const N = Number(input[0]);
 const M = Number(input[1]);
-const arr = input.slice(2, M + 2).map((v) => v.split(" ").map(Number));
+
+const graph = Array.from({ length: N + 1 }, () => Array(N + 1).fill(Infinity));
+
+for (let i = 2; i < M + 2; i++) {
+  const [start, end, cost] = input[i].split(" ").map(Number);
+  if (graph[start][end] === null || graph[start][end] > cost) {
+    graph[start][end] = cost;
+  }
+}
+
 const [start, end] = input[M + 2].split(" ").map(Number);
 
-const INF = 100000000;
-const graph = Array.from({ length: N + 1 }, () => []);
-const distance = Array(N + 1).fill(INF);
+const distance = new Array(N + 1).fill(Infinity);
+const visited = new Array(N + 1).fill(false);
 
-for (const [u, v, cost] of arr) {
-  graph[u].push([v, cost]);
-}
+const nextNode = () => {
+  let min = Infinity;
+  let minIdx = null;
 
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  push(node) {
-    this.heap.push(node);
-    this.bubbleUp();
-  }
-
-  pop() {
-    if (this.heap.length === 1) return this.heap.pop();
-    const min = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this.bubbleDown();
-    return min;
-  }
-
-  bubbleUp() {
-    let index = this.heap.length - 1;
-    const last = this.heap[index];
-    while (index) {
-      const parentIndex = Math.floor((index - 1) / 2);
-      const parent = this.heap[parentIndex];
-      if (last[0] >= parent[0]) break;
-      this.heap[index] = parent;
-      index = parentIndex;
-    }
-    this.heap[index] = last;
-  }
-
-  bubbleDown() {
-    let index = 0;
-    const length = this.heap.length;
-    const node = this.heap[index];
-
-    while (true) {
-      const leftIndex = 2 * index + 1;
-      const rightIndex = 2 * index + 2;
-      let swap = null;
-
-      if (leftIndex < length) {
-        const leftChild = this.heap[leftIndex];
-        if (leftChild[0] < node[0]) {
-          swap = leftIndex;
-        }
-      }
-
-      if (rightIndex < length) {
-        const rightChild = this.heap[rightIndex];
-        if (
-          (swap === null && rightChild[0] < node[0]) ||
-          (swap !== null && rightChild[0] < this.heap[swap][0])
-        ) {
-          swap = rightIndex;
-        }
-      }
-
-      if (swap === null) break;
-      this.heap[index] = this.heap[swap];
-      index = swap;
-    }
-    this.heap[index] = node;
-  }
-
-  size() {
-    return this.heap.length;
-  }
-}
-
-const dijkstra = (start) => {
-  const minHeap = new MinHeap();
-  distance[start] = 0;
-  minHeap.push([0, start]);
-
-  while (minHeap.size()) {
-    const [dist, current] = minHeap.pop();
-
-    if (distance[current] < dist) continue;
-
-    for (const [next, cost] of graph[current]) {
-      const newDist = dist + cost;
-      if (newDist < distance[next]) {
-        distance[next] = newDist;
-        minHeap.push([newDist, next]);
-      }
+  for (let i = 1; i <= N; i++) {
+    if (!visited[i] && distance[i] < min) {
+      min = distance[i];
+      minIdx = i;
     }
   }
+  return minIdx;
 };
 
-dijkstra(start);
+distance[start] = 0;
+
+let cur = start;
+
+for (let i = 0; i < N; i++) {
+  visited[cur] = true;
+
+  for (let j = 1; j <= N; j++) {
+    if (graph[cur][j]!==Infinity && graph[cur][j] + distance[cur] < distance[j]) {
+      distance[j] = graph[cur][j] + distance[cur];
+    }
+  }
+
+  if (nextNode()) {
+    cur = nextNode();
+  } else {
+    break;
+  }
+}
+
 console.log(distance[end]);
